50b78488fc345efdc5c426a82376b42eb2021c09
Leo-Ubuntu <Leon199407@gmail.com>
2016-11-08 15:17:48 +0800

    transfer

---
+++ dynamic_form/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DynamicFormConfig(AppConfig):
+    name = 'dynamic_form'
---
+++ dynamic_form/forms.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+__author__ = 'tommy.yu'
+
+from django.forms import ModelForm,Textarea
+from models import  *
+from django.forms.models import inlineformset_factory,modelform_factory
+
+class todo_listForm(ModelForm):
+    class Meta:
+        model = todo_list
+	fields = "__all__"
+        # widgets = {
+        #     'name': Textarea(attrs={'cols': 80, 'rows': 20}),
+        # }#yes,it works
+
+# class todo_itemForm(ModelForm):
+#     class Meta:
+#         model = todo_item
+#         exclude = ('list',)
+
+todo_itemSet = inlineformset_factory(todo_list, todo_item, fields="__all__")
+#modelform_factory()
